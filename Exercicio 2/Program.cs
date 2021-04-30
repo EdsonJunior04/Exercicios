f41cf082749420884167848e166f2a1f511a5d01
Edson Junior <ejr_04@outlook.com>
2021-04-30 19:28:34 -0300

    primeiro commit

---
+++ Exercicio 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o ano de nascimento ?");
+            int anoNascimento = Convert.ToInt32(Console.ReadLine());
+            int anoAtual = DateTime.Now.Year;
+
+            int idade = anoAtual - anoNascimento;
+            int bissexto = idade / 4;
+            int idadeDias = idade * 365 + bissexto;
+            int idadeSemana = idadeDias/7;
+
+            Console.WriteLine($"Você tem {idade} anos.\nE {idadeSemana} semanas");
+        }
+    }
+}
---
+++ Exercicio 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o salario?");
+            float salario = Convert.ToUInt32(Console.ReadLine());
+
+            if (salario < 500)
+            {
+                salario = salario * 1.3f;
+                Console.WriteLine($"O salario foi reajustado, passou a ser R${salario}.");
+            }else 
+            {
+                Console.WriteLine($"O salario não sofreu reajuste, continuara sendo R${salario}.");
+            }
+        }
+    }
+}
